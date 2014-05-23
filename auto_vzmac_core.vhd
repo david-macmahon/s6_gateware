2af7db3e6ec676a55f535e9dd0f4a69cb17ec141
David MacMahon <davidm@astro.berkeley.edu>
2014-05-23 12:57:19 -0700

    Add vzmac netlist and black box related files
    Add the actual netlists for auto_vzmac_core and cross_vzmac_core, plus the
    files needed to black box these two cores.

---
+++ auto_vzmac_core.vhd
@@ -0,0 +1,19 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity auto_vzmac_core is
+  port (
+    ce_1: in std_logic; 
+    clk_1: in std_logic; 
+    im0: in std_logic_vector(7 downto 0); 
+    re0: in std_logic_vector(7 downto 0); 
+    sync: in std_logic; 
+    re: out std_logic_vector(19 downto 0); 
+    valid: out std_logic
+  );
+end auto_vzmac_core;
+
+architecture structural of auto_vzmac_core is
+begin
+end structural;
+
---
+++ cross_vzmac_core.vhd
@@ -0,0 +1,22 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity cross_vzmac_core is
+  port (
+    ce_1: in std_logic; 
+    clk_1: in std_logic; 
+    im0: in std_logic_vector(7 downto 0); 
+    im1: in std_logic_vector(7 downto 0); 
+    re0: in std_logic_vector(7 downto 0); 
+    re1: in std_logic_vector(7 downto 0); 
+    sync: in std_logic; 
+    im: out std_logic_vector(20 downto 0); 
+    re: out std_logic_vector(20 downto 0); 
+    valid: out std_logic
+  );
+end cross_vzmac_core;
+
+architecture structural of cross_vzmac_core is
+begin
+end structural;
+
