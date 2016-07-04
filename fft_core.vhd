f575cd0fc0a77ca6dedfeed4c972bd6170e35e8f
David MacMahon <davidm@astro.berkeley.edu>
2016-07-04 09:59:34 -0700

    Generate fft_core.dcp and related files
    Also tweak .gitignore for ngc/dcp file.

--- fft_core.vhd
+++ fft_core.vhd
@@ -3,13 +3,13 @@ use IEEE.std_logic_1164.all;
 
 entity fft_core is
   port (
-    ce_1: in std_logic; 
-    clk_1: in std_logic; 
-    din: in std_logic_vector(39 downto 0); 
-    shift: in std_logic_vector(31 downto 0); 
-    sync_in: in std_logic; 
-    dout: out std_logic_vector(71 downto 0); 
-    sync_out: out std_logic
+    ce : in std_logic;
+    din : in std_logic_vector( 39 downto 0 );
+    shift : in std_logic_vector( 31 downto 0 );
+    sync_in : in std_logic;
+    clk : in std_logic;
+    dout : out std_logic_vector( 71 downto 0 );
+    sync_out : out std_logic
   );
 end fft_core;
 
