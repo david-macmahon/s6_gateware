02a8b20103d08974f32f56807b62e689d5544e79
David MacMahon <davidm@astro.berkeley.edu>
2016-07-04 05:48:50 -0700

    Generate cross_vzmac_core.dcp and related files
    Also tweak .gitignore for ngc/dcp file.

--- cross_vzmac_core.vhd
+++ cross_vzmac_core.vhd
@@ -3,16 +3,16 @@ use IEEE.std_logic_1164.all;
 
 entity cross_vzmac_core is
   port (
-    ce_1: in std_logic; 
-    clk_1: in std_logic; 
-    im0: in std_logic_vector(7 downto 0); 
-    im1: in std_logic_vector(7 downto 0); 
-    re0: in std_logic_vector(7 downto 0); 
-    re1: in std_logic_vector(7 downto 0); 
-    sync: in std_logic; 
-    im: out std_logic_vector(20 downto 0); 
-    re: out std_logic_vector(20 downto 0); 
-    valid: out std_logic
+    ce : in std_logic;
+    im0 : in std_logic_vector( 7 downto 0 );
+    im1 : in std_logic_vector( 7 downto 0 );
+    re0 : in std_logic_vector( 7 downto 0 );
+    re1 : in std_logic_vector( 7 downto 0 );
+    sync : in std_logic;
+    clk : in std_logic;
+    im : out std_logic_vector( 20 downto 0 );
+    re : out std_logic_vector( 20 downto 0 );
+    valid : out std_logic
   );
 end cross_vzmac_core;
 
